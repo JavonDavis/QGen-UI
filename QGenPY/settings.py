22fa2574c4cd6166a442e5b1e6bc6874badbdca1
Javon Davis <javonldavis14@gmail.com>
2017-05-21 02:09:40 -0500

    changed debug to false and branching off master

--- QGenPY/settings.py
+++ QGenPY/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9fxxg-0o4nf6k*+-4sm8m#=g!_f+vpgl8vni$4am!i#g*nq86a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
